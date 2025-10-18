4cd69e1fea8b499b6c8a6091b87c9af617370a2d
Weihan Zhou <Bob_Zhouweihan@outlook.com>
2025-10-19 03:24:30 +0800

    Implement subtract method

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -7,8 +7,11 @@ public class Calc {
         return x+y;
     }
 
-    public int subtract(int x, int y)
+    /*public int subtract(int x, int y)
     {
         return x*y;
+    }*/
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
