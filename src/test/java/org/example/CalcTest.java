3a724151c66a9428abbdc9940d20db3569e68105
Weihan Zhou <Bob_Zhouweihan@outlook.com>
2025-10-19 03:16:23 +0800

    Add failing subtraction test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2));
+    }
 
 }
